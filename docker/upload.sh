85f5079a965b742d8cdf28d82a840bbf0ee0e164
CiaoLee (Win) <ciao.junhyeok@gmail.com>
2023-10-13 17:32:52 +0900

    1. update build scripts

--- docker/upload.sh
+++ docker/upload.sh
@@ -1,9 +1,6 @@
-TAG1="docker.dev-friends.com/mango:latest"
-TAG2="docker.dev-friends.com/mango:5"
+TAG1="docker.dev-friends.com/mango:5"
 
 docker build \
   -t "$TAG1" \
-  -t "$TAG2" \
   -f ./mango5.dockerfile .
 docker push "$TAG1"
-docker push "$TAG2"
