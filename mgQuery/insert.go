63ee78794246fa6ee1c927933ada5d0fab9d1999
d3v_friends <d3v.friends@gmail.com>
2025-03-27 13:56:53 +0900

    1.0.8 bugfix 1) mgQuery.InsertOne, mgQuery.InsertMany

--- mgQuery/insert.go
+++ mgQuery/insert.go
@@ -12,13 +12,16 @@ const (
 	ErrEmptyModels = "empty_models"
 )
 
+// InsertOne
+// T 제네릭을 명시하지 않으면 new(T) 의 T 를 정확하게 인식 못한다. (golang 의 문제, 또는 golang 의 제네릭 이해의 문제)
+// 다른 query helper 들은 명시적으로 입력해야 하는 만큼 제네릭타입의 new 가 가능함
 func InsertOne[T mango.Model](
 	ctx context.Context,
 	model T,
 ) (err error) {
 	var col *mongo.Collection
 
-	if col, err = mgCtx.GetCol(ctx, *new(T)); err != nil {
+	if col, err = mgCtx.GetCol(ctx, model); err != nil {
 		return
 	}
 
@@ -45,7 +48,7 @@ func InsertMany[T mango.Model](
 	}
 
 	var col *mongo.Collection
-	if col, err = mgCtx.GetCol(ctx, *new(T)); err != nil {
+	if col, err = mgCtx.GetCol(ctx, models[0]); err != nil {
 		return
 	}
 
