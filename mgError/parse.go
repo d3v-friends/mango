7eea4af230557098605709a51b82fdbce2818d75
ciaolee <ciaolee@d3v-friends.com>
2025-06-12 10:50:53 +0900

    1.1.2

--- mgError/parse.go
+++ mgError/parse.go
@@ -1,11 +1,23 @@
 package mgError
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/d3v-friends/go-tools/fnError"
+)
 
 func Parse(err error) error {
 	switch err.Error() {
 	case errNotFoundModel:
 		return fmt.Errorf(ErrNotFoundModel)
+	}
+	return err
+}
+
+func ChangeError(err error, parser map[error]error) error {
+	for i := range parser {
+		if parser[i].Error() == err.Error() {
+			return fnError.NewF(parser[i].Error())
+		}
 	}
 	return err
 }
