547d5f831c5ac04a4c0eb458f6eacf1a89a35cd3
ciaolee <ciaolee@d3v-friends.com>
2025-06-17 13:40:18 +0900

    1.1.4

--- mgOp/types.go
+++ mgOp/types.go
@@ -2,16 +2,10 @@ package mgOp
 
 import "fmt"
 
-type Field string
-
-func (x Field) Ref() string {
-	return fmt.Sprintf("$%s", x)
+func Ref(v string) string {
+	return fmt.Sprintf("$%s", v)
 }
 
-func (x Field) SystemRef() string {
-	return fmt.Sprintf("$$%s", x)
-}
-
-func (x Field) String() string {
-	return string(x)
+func DoubleRef(v string) string {
+	return fmt.Sprintf("$$%s", v)
 }
